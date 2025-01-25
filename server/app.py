0c7a2b4691f8cafc461eeaa761d95b3a28ab93be
Quienten <quientenmiller2@gmail.com>
2025-01-25 12:39:20 -0800

    /agent/ route with search

--- server/app.py
+++ server/app.py
@@ -1,5 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
+from dotenv import load_dotenv
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Create flask app
 app = Flask(__name__)
@@ -9,10 +14,20 @@ CORS(app)
 
 
 # app routes for the backend
-@app.route('/api/endpoint', methods=['GET'])
+@app.route('     ', methods=['GET'])
 def home():
     return "hello world"
 
+@app.route('/agent/', methods=['POST'])
+def agent():
+    search = TavilySearchResults(max_results=2)
+    #search_results = search.invoke("what is the weather in SF")
+    search_results = search.invoke("What is a mortgage?")
+    print(search_results)
+    # If we want, we can create other tools.
+    # Once we have all the tools we want, we can put them in a list that we will reference later.
+    tools = [search]
+    return search_results
 
 if __name__ == '__main__':
     app.run(debug=True)
