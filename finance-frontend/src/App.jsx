3411ef1a201cecade05b2b3a8480c0d63a940179
Zaidaan <waszashibu@gmail.com>
2025-01-25 13:13:26 -0800

    chatbox

--- finance-frontend/src/App.jsx
+++ finance-frontend/src/App.jsx
@@ -5,29 +5,25 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [text, setText] = useState("")
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Title</h1>
+      <h2>Subtitle</h2>
+      <div className="textbox-container">
+        <textarea 
+          class = "chatbox"
+          placeholder="Message [AI APP NAME]..." 
+          value={text} 
+          onChange={(e) => setText(e.target.value)} 
+          rows="4" 
+          cols="50"
+        />
+        <button class="send-button">Send</button>
+
+        <p>You typed: {text}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
